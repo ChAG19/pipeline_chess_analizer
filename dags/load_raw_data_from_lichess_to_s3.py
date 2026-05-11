ac08433453d3a168d015762fe7541efe5a0ed373
Artem Chumachenko <artem.chumachenko45@gmail.com>
2026-05-11 15:40:53 +0300

    Переход дага загрузки из личес на taskflow

--- dags/load_raw_data_from_lichess_to_s3.py
+++ dags/load_raw_data_from_lichess_to_s3.py
@@ -6,6 +6,7 @@ from airflow.models.dag import DAG
 from airflow.models.variable import Variable
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 
 from services.MinioLoader import MinioLoader
 
@@ -30,58 +31,13 @@ SHORT_DESCRIPTION = "Загрузка данных lichess.com в s3"
 
 # Пользователь для анализа
 USERNAME = Variable.get("lichess_username")
+
 args = {
     "owner": OWNER,
     "catchup":True,
     "retries":2,
     "retry_delay": timedelta(minutes=1)
 }
-
-# Основная функция, инициализирует загрузку и передачу данных
-def load_raw_data_from_lichess_to_s3(**context):
-    start, end = get_loading_time(**context)
-    chess_data = get_file_from_lichess(start, end, **context)
-    if chess_data is not None:
-        if len(chess_data) == 0:
-            logging.info("Получен пустой файл.")
-        else:
-            logging.info("Файл содержит данные.")
-            load_data_to_s3(file=chess_data, date=start, **context) # Вызов функции по загурзке данных в s3
-
-# Функция отправляет файл в s3
-def load_data_to_s3(file, date: DateTime, **context):
-
-    conn = MinioLoader(minio_conn=MINIO_CONNECTION, bucket_name=BUCKET)
-
-    loading_date: DateTime = context["logical_date"]
-    file_name = loading_date.format("DD_MM_YYYY_HH_mm_ss") + ".json"
-    file_path = f"{LAYER}/{SOURCE}/{date.format("DD_MM_YYYY")}/{file_name}"
-
-    logging.info("Начало загрузки файла в s3 хранилище")
-    conn.upload(file=file, file_path=file_path)
-    logging.info(f"Файл {file_name} загружен\n{file_path}")
-
-# Получение файла с партиями
-# Вход: **context
-# Выход: полученный файл виде строки или None
-def get_file_from_lichess(start: DateTime, end: DateTime, **context):
-    logging.info(f"Запущена загрузка данных за {start.date()} из {SOURCE}")
-    
-    try:
-        response = requests.get(
-            url=f"https://lichess.org/api/games/user/{USERNAME}", 
-            params={'since': start.int_timestamp*1000, "until": end.int_timestamp*1000, 'opening':'true'}, # Умножение на 1000 нужно для нужного формата для Lichess
-            headers={'Accept':'application/x-ndjson'}
-        )
-    except requests.exceptions.RequestException as e:
-        logging.info("Ошибка соединения")
-    
-    if response.ok:
-        logging.info(f"Данные за {start.date()} получены.")
-        return response.text
-    
-    logging.info("Ошибка. Данные не получены!")
-    return None
 
 # Получение диапозона загрузки
 # Вход: data_interval_end (внутри **context)
@@ -94,48 +50,59 @@ def get_loading_time(**context) -> tuple[DateTime, DateTime]:
     start_timestamp = start_timestamp.start_of('day')
     return start_timestamp, end_timestamp
 
-with DAG(
+@dag(
     default_args=args,
     dag_id=DAG_ID,
     description=SHORT_DESCRIPTION,
-    schedule=timedelta(days=1),
-    start_date=datetime(2026, 3, 16),
+    schedule="@daily",
+    start_date=datetime(2026, 5, 9),
     catchup=False,
     concurrency=1,
     max_active_runs=1,
     max_active_tasks=1, 
-    tags=["raw", "s3", "lichess"],
-) as dag:
+    tags=["raw", "s3", "lichess"]
+)
 
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    t1 = EmptyOperator(
-        task_id = "Start"
-    )
-    t1.doc_md = textwrap.dedent(
-        """
-    #### Запуск Dag
-    Запуск дага
-    """
-    )
+def load_raw_data_from_lichess_to_s3():
+    # Получение файла с партиями
+    # Вход: **context
+    # Выход: полученный файл виде строки или None
+    @task()
+    def get_file_from_lichess(**context):
+        start, end = get_loading_time(**context)
+        
+        logging.info(f"Запущена загрузка данных за {start.date()} из {SOURCE}")
+        
+        try:
+            response = requests.get(
+                url=f"https://lichess.org/api/games/user/{USERNAME}", 
+                params={'since': start.int_timestamp*1000, "until": end.int_timestamp*1000, 'opening':'true'}, # Умножение на 1000 нужно для нужного формата для Lichess
+                headers={'Accept':'application/x-ndjson'}
+            )
+        except requests.exceptions.RequestException as e:
+            logging.info("Ошибка соединения")
+        
+        logging.info(f"Данные за {start.date()} получены.")
+        return response.text
+        
     
-    t2 = PythonOperator(
-        task_id="load_raw_data_from_lichess_to_s3",
-        python_callable=load_raw_data_from_lichess_to_s3
-    )
-    t2.doc_md = textwrap.dedent(
-        """\
-    #### Запуск Dag
-    Основной шаг, который скачивает файл из lichess api в Minio хранилище
-    """
-    )
+    @task.run_if(lambda chess_data: len(chess_data) != 0)
+    
+    @task()
+    # Функция отправляет файл в s3
+    def load_data_to_s3(file, **context):
+        date: DateTime = context["data_interval_start"]
+        conn = MinioLoader(minio_conn=MINIO_CONNECTION, bucket_name=BUCKET)
 
-    t3 = EmptyOperator(
-        task_id="End",
-    )
-    t3.doc_md = textwrap.dedent(
-        """
-    #### Запуск Dag
-    Окончание дага
-    """
-    )
-    t1 >> t2 >> t3
+        loading_date: DateTime = context["logical_date"]
+        file_name = loading_date.format("DD_MM_YYYY_HH_mm_ss") + ".json"
+        file_path = f"{LAYER}/{SOURCE}/{date.format("DD_MM_YYYY")}/{file_name}"
+
+        logging.info("Начало загрузки файла в s3 хранилище")
+        conn.upload(file=file, file_path=file_path)
+        logging.info(f"Файл {file_name} загружен\n{file_path}")
+
+    file = get_file_from_lichess()
+    load_data_to_s3(file)
+
+load_raw_data_from_lichess_to_s3()
